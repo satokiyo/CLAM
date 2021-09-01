8b1007b12da5c56e4aaeb97b05ca8e1d27f39e4a
satokiyo <k.sato.ontology@gmail.com>
2021-09-01 02:22:58 +0000

    fea/seg_overlay_only

--- 0823_run_clam_TMUCHN_test.sh
+++ 0823_run_clam_TMUCHN_test.sh
@@ -37,24 +37,24 @@
 #--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-512_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0/0824-091053/best_model_3.pth
 
 
-## se_resnext no copy paste 768
-#python3 create_patches_fp.py \
-#--source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
-#--source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
-#--save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0719pdl1TMU_40xRename_7class_annotation/se_resnext_no_copy_paste_768 \
-#--patch_size 1536  \
-#--step_size 1024   \
-#--patch_resolution_detection 20  \
-#--patch_resolution_segmentation 10  \
-#--seg  \
-#--patch  \
-#--stitch   \
-#--no_auto_skip  \
-#--intensity_thres 176 \
-#--area_thres 0.05 \
-#--radius 16 \
-#--ckpts_detection /media/prostate/20210331_PDL1/CLAM/models/nuclei_detection/ckpts/20x/0716-233238/best_model_1.pth \
-#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-768_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0/0825-124534/best_model_3.pth
+# se_resnext no copy paste 768
+python3 create_patches_fp.py \
+--source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
+--source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
+--save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0719pdl1TMU_40xRename_7class_annotation/se_resnext_no_copy_paste_768_tmp \
+--patch_size 1536  \
+--step_size 1024   \
+--patch_resolution_detection 20  \
+--patch_resolution_segmentation 10  \
+--seg  \
+--patch  \
+--stitch   \
+--no_auto_skip  \
+--intensity_thres 176 \
+--area_thres 0.05 \
+--radius 16 \
+--ckpts_detection /media/prostate/20210331_PDL1/CLAM/models/nuclei_detection/ckpts/20x/0716-233238/best_model_1.pth \
+--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-768_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0_ssl-0/0830-062514/best_model_5.pth
 
 #--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-768_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0/0824-191135/best_model_5.pth
 
@@ -97,11 +97,36 @@
 #--ckpts_segmentation 
 
 
-## CHN se-resnext no paste 768
+### CHN se-resnext no paste 768
+##python3 create_patches_fp.py \
+##--source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210721pdl1CHN/40xRename/PDL1 \
+##--source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210721pdl1CHN/40xRename/0823_test \
+##--save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0721pdl1CHN_40xRename_7class_annotation/se_resnext_no_copy_paste_768 \
+##--patch_size 1536  \
+##--step_size 1024   \
+##--patch_resolution_detection 20  \
+##--patch_resolution_segmentation 10  \
+##--seg  \
+##--patch  \
+##--stitch   \
+##--no_auto_skip  \
+##--intensity_thres 176 \
+##--area_thres 0.05 \
+##--radius 16 \
+##--ckpts_detection /media/prostate/20210331_PDL1/CLAM/models/nuclei_detection/ckpts/20x/0716-233238/best_model_1.pth \
+##--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-768_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0_ssl-0/0830-062514/best_model_5.pth
+
+#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-768_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0/0825-124534/best_model_3.pth
+
+#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-768_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0/0824-191135/best_model_5.pth
+
+
+### change branch seg_overlay only , config ssl = 0
+## TMU se-resnext no copy paste 512x512 SSL
 #python3 create_patches_fp.py \
-#--source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210721pdl1CHN/40xRename/PDL1 \
-#--source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210721pdl1CHN/40xRename/0823_test \
-#--save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0721pdl1CHN_40xRename_7class_annotation/se_resnext_no_copy_paste_768 \
+#--source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
+#--source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
+#--save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0719pdl1TMU_40xRename_7class_annotation/se_resnext_no_copy_paste_512 \
 #--patch_size 1536  \
 #--step_size 1024   \
 #--patch_resolution_detection 20  \
@@ -114,48 +139,50 @@
 #--area_thres 0.05 \
 #--radius 16 \
 #--ckpts_detection /media/prostate/20210331_PDL1/CLAM/models/nuclei_detection/ckpts/20x/0716-233238/best_model_1.pth \
-#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-768_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0/0825-124534/best_model_3.pth
-
-#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-768_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0/0824-191135/best_model_5.pth
-
-# TMU se-resnext no copy paste 512x512 SSL
-python3 create_patches_fp.py \
---source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
---source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
---save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0719pdl1TMU_40xRename_7class_annotation/se_resnext_no_copy_paste_SSL \
---patch_size 1536  \
---step_size 1024   \
---patch_resolution_detection 20  \
---patch_resolution_segmentation 10  \
---seg  \
---patch  \
---stitch   \
---no_auto_skip  \
---intensity_thres 176 \
---area_thres 0.05 \
---radius 16 \
---ckpts_detection /media/prostate/20210331_PDL1/CLAM/models/nuclei_detection/ckpts/20x/0716-233238/best_model_1.pth \
---ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-512_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0_ssl-1/0826-151549/best_model_6.pth
+#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-512_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0_ssl-0/0827-130644/best_model_2.pth 
 
 
-# CHN se-resnext no copy paste 512x512 SSL
-python3 create_patches_fp.py \
---source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210721pdl1CHN/40xRename/PDL1 \
---source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210721pdl1CHN/40xRename/0823_test \
---save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0721pdl1CHN_40xRename_7class_annotation/se_resnext_no_copy_paste_SSL \
---patch_size 1536  \
---step_size 1024   \
---patch_resolution_detection 20  \
---patch_resolution_segmentation 10  \
---seg  \
---patch  \
---stitch   \
---no_auto_skip  \
---intensity_thres 176 \
---area_thres 0.05 \
---radius 16 \
---ckpts_detection /media/prostate/20210331_PDL1/CLAM/models/nuclei_detection/ckpts/20x/0716-233238/best_model_1.pth \
---ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-512_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0_ssl-1/0826-151549/best_model_6.pth
+### calculate TPS
+### change branch master , config ssl = 0
+## TMU se-resnext no copy paste 512x512 
+#python3 create_patches_fp.py \
+#--source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
+#--source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210719pdl1TMU/40xRename/PDL1 \
+#--save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0719pdl1TMU_40xRename_7class_annotation/se_resnext_no_copy_paste_512 \
+#--patch_size 1536  \
+#--step_size 1024   \
+#--patch_resolution_detection 20  \
+#--patch_resolution_segmentation 10  \
+#--seg  \
+#--patch  \
+#--stitch   \
+#--no_auto_skip  \
+#--intensity_thres 176 \
+#--area_thres 0.05 \
+#--radius 16 \
+#--ckpts_detection /media/prostate/20210331_PDL1/CLAM/models/nuclei_detection/ckpts/20x/0716-233238/best_model_1.pth \
+#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-512_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0_ssl-0/0828-195028/best_model_5.pth
+
+#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-512_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0_ssl-1/0826-151549/best_model_6.pth
+
+## CHN se-resnext no copy paste 512x512 SSL
+#python3 create_patches_fp.py \
+#--source_slides /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210721pdl1CHN/40xRename/PDL1 \
+#--source_annotations /media/HDD2/20210331_PDL1/data/ndpi/2021pdl1/20210721pdl1CHN/40xRename/0823_test \
+#--save_dir /media/HDD2/20210331_PDL1/CLAM/20210823_0721pdl1CHN_40xRename_7class_annotation/se_resnext_no_copy_paste_SSL \
+#--patch_size 1536  \
+#--step_size 1024   \
+#--patch_resolution_detection 20  \
+#--patch_resolution_segmentation 10  \
+#--seg  \
+#--patch  \
+#--stitch   \
+#--no_auto_skip  \
+#--intensity_thres 176 \
+#--area_thres 0.05 \
+#--radius 16 \
+#--ckpts_detection /media/prostate/20210331_PDL1/CLAM/models/nuclei_detection/ckpts/20x/0716-233238/best_model_1.pth \
+#--ckpts_segmentation /media/prostate/20210331_PDL1/segmentation/DM-Count/ckpts/encoder-se_resnext50_32x4d_input-512_downrate-1_scale_pyramid_module-1_attention_branch-0_albumentation-1_copy_paste-0_deep_supervision-1_ocr-0_ssl-1/0826-151549/best_model_6.pth
 
 
 
